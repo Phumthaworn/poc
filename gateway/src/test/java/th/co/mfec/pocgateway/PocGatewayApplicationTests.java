b491ecb04892828cb2622267ac065099ad66f120
amornthep <amornthep.jay@gmail.com>
2021-09-02 16:22:37 +0700

    commit gateway

---
+++ gateway/src/test/java/th/co/mfec/pocgateway/PocGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.mfec.pocgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
