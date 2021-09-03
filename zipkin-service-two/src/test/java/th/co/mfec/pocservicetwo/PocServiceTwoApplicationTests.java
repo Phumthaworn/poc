d20f56ac312b3fe9481f1cbaa69347b76d8c295a
amornthep <amornthep.jay@gmail.com>
2021-09-03 13:18:56 +0700

    commit zipkin service

---
+++ zipkin-service-two/src/test/java/th/co/mfec/pocservicetwo/PocServiceTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.mfec.pocservicetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocServiceTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
